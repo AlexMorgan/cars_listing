5139ad3cb9cd7fabcb0816d6b03ffab8c80df823
Helen Hood <helen.c.hood@gmail.com>
2014-07-03 13:58:59 -0400

    Remove whitespace, fix spec name

--- spec/features/user_comes_to_root_page_with_list_of_cars_spec.rb
+++ spec/features/user_comes_to_root_page_with_list_of_cars_spec.rb
@@ -1,15 +1,12 @@
 require 'rails_helper'
 
-feature 'user visits home page and sees a list of all cars' do
-  scenario 'user sees a list of cars' do
+feature 'user visits home page and sees a list of all manufacturers' do
+  scenario 'user sees a list of manufacturers' do
     manufacturers = FactoryGirl.create_list(:manufacturer, 20)
-
     visit root_path
-
 
     manufacturers.each do |m|
       expect(page).to have_content(m.name)
     end
-
   end
 end
