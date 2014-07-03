e53f8de689003e9014f45a3b64272cf1704892c1
Helen Hood <helen.c.hood@gmail.com>
2014-07-03 13:52:50 -0400

    Modify car validations

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,7 +2,17 @@ class Car < ActiveRecord::Base
   belongs_to :manufacturer
 
   validates :color, presence: true
-  validates :year, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1920 }
-  validates :mileage, presence: true, numericality: { only_integer: true }
-  validates :manufacturer_id, presence: true
+  validates :year, presence: true,
+    numericality: {
+      only_integer: true,
+      greater_than_or_equal_to: 1920,
+      less_than_or_equal_to: Time.now.year
+    }
+  validates :mileage,
+    presence: true,
+    numericality: {
+      only_integer: true,
+      greater_than_or_equal_to: 0
+    }
+  validates :manufacturer, presence: true
 end
